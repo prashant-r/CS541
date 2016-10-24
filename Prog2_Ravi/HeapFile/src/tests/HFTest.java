45bd417658a79bed12e1bef28ac6ca0af07f4e3e
Prashant Ravi <prashant.ravi@gmail.com>
2016-10-24 14:21:35 -0400

    Test 2 passes ..

--- Prog2_Ravi/HeapFile/src/tests/HFTest.java
+++ Prog2_Ravi/HeapFile/src/tests/HFTest.java
@@ -799,7 +799,7 @@ class HFDriver extends TestDriver implements GlobalConst
 		boolean _passAll = OK;
 
 		if (!test1()) { _passAll = FAIL; }
-//		if (!test2()) { _passAll = FAIL; }
+		if (!test2()) { _passAll = FAIL; }
 //		if (!test3()) { _passAll = FAIL; }
 //		if (!test4()) { _passAll = FAIL; }
 		//if (!test5()) { _passAll = FAIL; }		
